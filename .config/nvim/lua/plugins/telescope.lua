e203211948e03ca93b117da5a53cc9639a8a6ef2
dorusvanschaik <dorusvschaik@gmail.com>
2025-06-20 20:48:52 +0200

    Added keybindings for telescope and toggleterm

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
-return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+return 
+{
+  { 
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys =
+    {
+      {'<leader>fb', "<cmd>Telescope buffers<cr>", desc = "Telescope buffers"},
+      {'<leader>ff', "<cmd>Telescope find_files<cr>", desc = "Telescope find file"},
+    },
+  },
+}
--- .config/nvim/lua/plugins/toggleterm-nvim.lua
+++ .config/nvim/lua/plugins/toggleterm-nvim.lua
@@ -1,1 +1,12 @@
-return {'akinsho/toggleterm.nvim', version = "*", config = true}
+return 
+{
+  { 
+    'akinsho/toggleterm.nvim', 
+    version = "*",
+    config = true,
+    keys =
+    {
+      {'<leader>tt', "<cmd>ToggleTerm direction=vertical size=80<cr>", desc = "ToggleTerm open terminal"},
+    },
+  },
+}
